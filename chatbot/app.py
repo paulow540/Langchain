5b0911b41857bf48834f8e8f5dfdc6a24b3d7e48
paulow540 <halleluyahpaul@gmail.com>
2025-08-06 14:31:00 +0100

    working

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,8 +1,39 @@
-# from langchain
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
 import streamlit as st
+import os
+from dotenv import load_dotenv
 
 
+
+
+
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+
+
+## Prompt Template
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant. Please response to the user queries"),
+        ("user", "Question:{question}")
+    ]
+)
 
 
 st.title("SQI CUSTOMER CHATBOT", anchor=False)
-st.chat_input("Ask me any question about SQI")
+input_text = st.input_text("Ask me any question about SQI")
+
+ 
+# OpenAI LLM
+llm = ChatOpenAI(model="gpt-3.5-turbo")
+output_parser =StrOutputParser()
+chain = prompt|llm|output_parser
+
+
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
